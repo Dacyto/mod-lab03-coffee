aee74f854588d0e8f1d37606688a4057fe9dcbd4
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-25 23:07:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -26,3 +26,46 @@ TEST(task3, test3) {
     states rez = newAutomata.getState();
     EXPECT_EQ(CHECK, rez);
 }
+
+TEST(task4, test4) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(130);
+    Nier.choice(4);
+    Nier.cook();
+    states rez = newAutomata.getState();
+    EXPECT_EQ(COOK, rez);
+}
+
+TEST(task5, test5) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(130);
+    Nier.choice(4);
+    Nier.cook();
+    Nier.finish();
+    states rez = newAutomata.getState();
+    EXPECT_EQ(WAIT, rez);
+}
+
+TEST(task6, test6) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(25);
+    Nier.choice(2);
+    Nier.cancel();
+    states rez = newAutomata.getState();
+    EXPECT_EQ(WAIT, rez);
+}
+
+TEST(task7, test7) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(130);
+    Nier.choice(4);
+    Nier.cook();
+    Nier.finish();
+    Nier.off();
+    states rez = newAutomata.getState();
+    EXPECT_EQ(OFF, rez);
+}
