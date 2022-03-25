af84d87a5748bfcebe77b37655e8054a4b54e592
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-25 22:56:25 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,27 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task1, test1) {
+  Automata Nier = Automata();
+  Nier.on();
+  STATES rez = Nier.getStates();
+  EXPECT_EQ(WAIT, rez);
+}
+
+TEST(task2, test2) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(95);
+    states rez = Nier.getState();
+    EXPECT_EQ(ACCEPT, rez);
+}
+
+TEST(task3, test3) {
+    Automata Nier = Automata();
+    Nier.on();
+    Nier.coin(130);
+    Nier.choice(4);
+    states rez = newAutomata.getState();
+    EXPECT_EQ(CHECK, rez);
+}
