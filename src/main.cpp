9d8f973f96702df7a4a1426af55d6a5ec3eb29ec
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-25 23:19:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@ using std::endl;
 
 int main() {
   Automata Vend; // Vending Machine
-  
+
   Vend.on();
   cout << "Vending Machine is on!" << endl;
-  
+
   Vend.getMenu();
   Vend.coin(100);
   cout << "money has been successfully deposited" << endl;
-  
+
   Vend.choice(4);
   if (Vend.check() == true) {
     Vend.cook();
@@ -24,11 +24,11 @@ int main() {
   } else {
     cout << "Error, try again!" << Vend.cancel() << endl;
   }
-  
+
   cout << "Take your change: " << Vend.finish() << endl;
-  
+
   Vend.off();
   cout << "Shutting Down Vending Machine..." << endl;
-  
+
   return 0;
 }
